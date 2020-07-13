23df136fe192c5da96968db1b68b667b6ea29576
vigan <vigan.abd@gmail.com>
2020-07-13 23:04:08 +0200

    bumped 0.5.0

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,4 @@ cp ./nuget.config.example ./nuget.config
 sed -i "s/__USER__/$1/g" ./nuget.config
 sed -i "s/__GITHUB_TOKEN__/$2/g" ./nuget.config
 dotnet pack --configuration Release
-dotnet nuget push bin/Release/Grenache.0.0.1.2.nupkg --source github
+dotnet nuget push bin/Release/Grenache.0.5.0.nupkg --source github
