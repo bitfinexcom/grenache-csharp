1c33fce892228493e7e6752984e3bbd0084ab860
vigan-abd <vigan.abd@gmail.com>
2024-04-19 19:09:31 +0200

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@ set -e
 
 BASEDIR=$(dirname "$0")
 
-if [[ $# -ne 2 ]]; then
-  echo "Usage: ./deploy.sh <git-user> <git-token>"
+if [[ $# -ne 3 ]]; then
+  echo "Usage: ./deploy.sh <version> <git-user> <git-token>"
+  echo "E.g.: ./deploy.sh 0.5.0 vigan-abd ghp_S1..."
   exit 1
 fi
 
@@ -12,7 +13,7 @@ dotnet restore
 dotnet build
 cd "$BASEDIR/Grenache"
 cp ./nuget.config.example ./nuget.config
-sed -i "s/__USER__/$1/g" ./nuget.config
-sed -i "s/__GITHUB_TOKEN__/$2/g" ./nuget.config
+sed -i "s/__USER__/$2/g" ./nuget.config
+sed -i "s/__GITHUB_TOKEN__/$3/g" ./nuget.config
 dotnet pack --configuration Release
-dotnet nuget push bin/Release/Grenache.0.5.0.nupkg --source github
+dotnet nuget push bin/Release/Grenache.$1.nupkg --source github
